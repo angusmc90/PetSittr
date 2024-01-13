a101f68dc813e359c5a65c4ddff09c35a0186636
Angus <34198794+angusmc90@users.noreply.github.com>
2024-01-13 15:45:27 -0600

    create theme folder to adjust elements being added on the fly

---
+++ src/react-semantic-theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from 'semantic-ui-react';
+
+const myTheme = createTheme({
+  primaryColor: 'blue',
+  textColor: 'white',
+  backgroundColor: 'lightgray',
+  fontFamily: 'sans-serif',
+  // ... other variables
+});
+
+export default myTheme;
