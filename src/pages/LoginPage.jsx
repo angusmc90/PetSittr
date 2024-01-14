9496ae2b0717bd797efd6fda688b8a81878644a8
Angus <34198794+angusmc90@users.noreply.github.com>
2024-01-14 12:33:11 -0600

    login page uses tabbed forms

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,20 +1,23 @@
 import { useState } from 'react'
 import 'semantic-ui-css/semantic.min.css'
+import { TabPane, Tab } from 'semantic-ui-react'
 
 import LoginForm from '../components/Forms/LoginForm';
 import SignupForm from '../components/Forms/SignupForm';
 
 export default function LoginPage() {
-  const [portal, setPortal] = useState('login')
-
+    const portals = [
+        { menuItem: 'Login', render: () => <TabPane><LoginForm/></TabPane> },
+        { menuItem: 'Signup', render: () => <TabPane><SignupForm/></TabPane> },
+      ]
+      
   function showForm () {
-    
+
   }
 
   return (
     <>
-    <LoginForm></LoginForm>
-    <SignupForm></SignupForm>
+    <Tab panes={portals}></Tab>
     </>
   )
 }
