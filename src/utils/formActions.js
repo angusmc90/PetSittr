aa605bd2d5ded8b659a46f310eb9f4c1659e1c50
Angus <34198794+angusmc90@users.noreply.github.com>
2024-01-13 18:56:45 -0600

    created a formActions util since dynamically rendering forms based on state wil be used in a few areas in the app

---
+++ src/utils/formActions.js
@@ -0,0 +1,20 @@
+function renderFields(e) {
+    // return KVPs as an array
+    const fieldsArr = Object.entries(e);
+    // for each index in the array, create a field
+    const fields = fieldsArr.map( ([fieldName, fieldVal]) => (
+        <FormField key={fieldName}>
+            <Input
+            type={fieldVal.type}
+            placeholder={fieldName}
+            name={fieldName}
+            value={fieldVal}
+            />
+      </FormField>
+    ))
+    return fields;
+}
+
+export default {
+    renderFields,
+}
