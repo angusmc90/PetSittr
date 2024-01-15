026e45679877e1e1e55d123d38f0a6cdbab090bd
Angus <34198794+angusmc90@users.noreply.github.com>
2024-01-15 17:07:31 -0600

    swap login page for feed page and import navbar compoonent

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 import 'semantic-ui-css/semantic.min.css'
 
+import NavBar from '../components/NavBar'
+
 export default function FeedPage(){
 
     return (
-        <></>
+        <>
+        <NavBar></NavBar>
+        </>
     )
 }
