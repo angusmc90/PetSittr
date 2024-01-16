57df9ff1e813cde5f4da854e04576f73700cb63e
Angus <34198794+angusmc90@users.noreply.github.com>
2024-01-16 13:25:37 -0600

    add palceholder txt for petbio

--- src/components/Bios/PetBio.jsx
+++ src/components/Bios/PetBio.jsx
@@ -4,15 +4,28 @@ import {
     TabPane, 
     Tab,
     Grid,
-    GridColumn 
+    GridColumn,
+    GridRow, 
+    Container
 } from 'semantic-ui-react'
 
 
 export default function PetBio() {
 
     return (
-        <>
-            This is the aside pet bio section
-        </>
+        <Container>
+            <Grid centered>
+                <GridRow>
+                    <GridColumn width={3}>
+                        PetPic
+                    </GridColumn>
+                    <GridColumn width={12}>
+                        Pet_Name_bold<br/>
+                        Pet_species, pet_age<br/>
+                        Pet_bio: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    </GridColumn>
+                </GridRow>
+            </Grid>
+        </Container>
     )
 }
