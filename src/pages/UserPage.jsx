aa4739647b70f7906014cb9adaa873614847286a
Angus <34198794+angusmc90@users.noreply.github.com>
2024-01-17 15:31:49 -0600

    computer about to die dont want to lose

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,35 +1,43 @@
-// import { useState } from 'react'
-// import {
-//     TabPane,
-//     Tab,
-//     Grid,
-//     GridColumn,
-//     GridRow
-// } from 'semantic-ui-react'
+import { useState } from 'react'
+import {
+    TabPane,
+    Tab,
+    Grid,
+    GridColumn,
+    GridRow
+} from 'semantic-ui-react'
 
-// import NavBar from '../components/NavBar';
-// import UserBio from '../components/Bios/UserBio';
+import NavBar from '../components/NavBar';
+import UserBio from '../components/Bios/UserBio';
 // import PetBio from '../components/Bios/PetBio'
 
-// export default function LoginPage() {
-//     const portals = [
-//         { menuItem: 'Login', render: () => <TabPane><LoginForm /></TabPane> },
-//         { menuItem: 'Signup', render: () => <TabPane><SignupForm /></TabPane> },
-//     ]
+export default function LoginPage() {
+    const [user, setUser] = useState(
+        {
+            username: 'userName',
+            pets: 'pets'
+        }
+    )
 
-//     return (
-//         <>
-//             <NavBar></NavBar>
-//             <Grid centered>
-//                 <GridRow>
-//                     <GridColumn width={4} stretched>
-//                         <UserBio></UserBio>
-//                     </GridColumn>
-//                     <GridColumn width={8} stretched>
-//                         <PetBio></PetBio>
-//                     </GridColumn>
-//                 </GridRow>
-//             </Grid>
-//         </>
-//     )
-// }
+    const [veiw, setView] = useState('pets');
+
+    return (
+        <>
+            <NavBar></NavBar>
+            <HStack>
+                <UserBio user={'placeholder'}/>
+                <PetBio pets={'placeholder'}/>
+            </HStack>
+            <Grid centered>
+                <GridRow>
+                    <GridColumn width={4} stretched>
+                        <UserBio></UserBio>
+                    </GridColumn>
+                    <GridColumn width={8} stretched>
+                        <PetBio></PetBio>
+                    </GridColumn>
+                </GridRow>
+            </Grid>
+        </>
+    )
+}
