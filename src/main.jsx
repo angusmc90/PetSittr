f63af847c6facacc2666b2c11de35658cc1102bc
Angus <34198794+angusmc90@users.noreply.github.com>
2024-01-18 12:46:43 -0600

    change router to browserrouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Router from 'react-router-dom'
+import BrowserRouter from 'react-router-dom'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-      <Router>
+      <BrowserRouter>
         <App />
-      </Router>
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>,
 )
