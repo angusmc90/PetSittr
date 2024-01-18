82429470bfc17802b00b1b4ecc5de0f62e44f13b
Angus <34198794+angusmc90@users.noreply.github.com>
2024-01-18 16:04:50 -0600

    create userEdit branch for a user edit form

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import BrowserRouter from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-      <BrowserRouter>
+      <Router>
         <App />
-      </BrowserRouter>
+      </Router>
     </ChakraProvider>
   </React.StrictMode>,
 )
