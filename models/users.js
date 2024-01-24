cb96fa5e06a2193fde1cbe45435260ca3ad52c56
Angus <34198794+angusmc90@users.noreply.github.com>
2024-01-24 17:33:53 -0600

    - built database.js doc to connect to mongo db #-updated the usermodel to indicate required fields and need unique fields #- created conditional logic to formActions to render different input types based on fieldVal.type

--- models/users.js
+++ models/users.js
@@ -4,12 +4,19 @@ const userSchema = new mongoose.Schema(
     {
         username: {
             type: String,
+            required: true,
+            lowercase: true,
+            unique: true
         },
         password: {
             type: String,
+            required: true,
         },
         email: {
             type: String,
+            required: true,
+            lowercase: true,
+            unique: true
         },
         preferredName: {
             type: String,
@@ -26,7 +33,7 @@ const userSchema = new mongoose.Schema(
         }],
         sits: {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Sitting',
+            ref: 'Sitts',
         },
     }, {
         timestamp: true,
--- src/utils/formActions.jsx
+++ src/utils/formActions.jsx
@@ -4,16 +4,26 @@ function renderFields(e, handleChange) {
     // return KVPs as an array
     const fieldsArr = Object.entries(e);
     // for each index in the array, create a field
-    const fields = fieldsArr.map(([fieldName, fieldVal]) => (
-        // come back to make this conditional for field types
-        console.log(fieldName),
-        console.log(fieldVal.value),
-        <Input key={fieldName} 
-            placeholder={fieldName} 
-            value={fieldVal.value}
-            onChange={(e)=> handleChange(fieldName, e.target.value)}/>
-    ))
-    return fields;
+    return fieldsArr.map(([fieldName, fieldVal]) => {
+        if (fieldVal.type === 'File') {
+            console.log('FILE FILE FILE')
+            console.log(fieldVal.type)
+            return (
+                <input key={fieldName}
+                    type='file'
+                    onChange={(e) => handleChange(fieldName, e.target.files[0])}/>
+            );
+        } else {
+            console.log(fieldVal.type)
+            return (
+                <Input key={fieldName}
+                    placeholder={fieldName}
+                    value={fieldVal.value}
+                    type="string"
+                    onChange={(e) => handleChange(fieldName, e.target.value)} />
+            );
+        }
+    });
 }
 
 export {
